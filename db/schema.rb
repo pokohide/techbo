3a121667ec7c6915c923f4522c4bbcff2e1f481a
pokohide <hyde31415@gmail.com>
2017-02-18 01:01:58 +0900

    entryをmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217155046) do
+ActiveRecord::Schema.define(version: 20170217155819) do
+
+  create_table "entries", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "is_draft"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
