ee30941ab22cd63d427e63bac5727647ea0a235a
pokohide <hyde31415@gmail.com>
2017-02-18 22:14:59 +0900

    画像表示

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -9,12 +9,13 @@ class Entry < ApplicationRecord
                     storage: :s3,
                     s3_credentials: "#{Rails.root}/config/s3.yml",
                     path: 'users/:style/:id.:extension',
+                    s3_permissions: :public,
                     url: ':s3_domain_url'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def image_url(style=nil, expires_in=90.minutes)
     if image.present?
-      image.s3_object(style).url_for(:read, secure: true, expires: expires_in).to_s
+      image.s3_object(style).url_for(:read, expires: expires_in).to_s
     else
       'https://s3-ap-northeast-1.amazonaws.com/beeapp-production/no-img.png'
     end
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,2 @@
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-ap-northeast-1.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
