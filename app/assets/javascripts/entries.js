0f7e80c7bbf9f210752c69ce3ef4675b1a7aa211
pokohide <hyde31415@gmail.com>
2017-02-19 04:30:13 +0900

    コメント投稿

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   $('#entry_tag_list').tagit()
   //$('#entry_is_draft').checkbox()
 
-  $('.reply').on('click', function(e) {
+  $('.comment-reply').on('click', function(e) {
     e.preventDefault()
     var reply = $(this).attr('data-reply')
     console.log(reply)
