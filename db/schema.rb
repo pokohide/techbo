75c2dcc266cf8e46b1c8bdc6dd9bb3cfb1092f39
pokohide <hyde31415@gmail.com>
2017-02-19 02:19:20 +0900

    userにname column追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218163145) do
+ActiveRecord::Schema.define(version: 20170218171845) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "entry_id"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20170218163145) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
