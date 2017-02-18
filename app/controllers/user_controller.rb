12b205428b34976f1df04fd3391f97bb8c10a361
pokohide <hyde31415@gmail.com>
2017-02-18 20:31:39 +0900

    mypage

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,13 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def me
+    if user_signed_in?
+      @user = current_user
+    else
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'user/show'
-
-  get 'user/me'
-
   root to: 'entries#index'
 
   resources :entries do
