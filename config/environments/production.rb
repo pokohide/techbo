1f26918e39fd7db7928d3214951ceb802c19aed9
pokohide <hyde31415@gmail.com>
2017-02-23 19:58:54 +0900

    default変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,16 +25,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    }
-  }
-
   Rails.application.routes.default_url_options[:host] = 'https://techbo.herokuapp.com/'
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
